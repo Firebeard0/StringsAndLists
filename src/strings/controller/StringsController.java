ba98e2ae64318691382b58a60af193fdfa1170af
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-10-03 13:39:03 -0600

    made the kahoots popup

--- src/strings/controller/StringsController.java
+++ src/strings/controller/StringsController.java
@@ -3,26 +3,39 @@ package strings.controller;
 import java.util.List;
 import java.util.ArrayList;
 import strings.model.Kahoot;
-
+import strings.view.StringDisplay;
 public class StringsController
 {
 	private List<Kahoot> myKahoots;
-	
+	private StringDisplay popup;
 	public StringsController()
 	{
 		myKahoots = new ArrayList<Kahoot>();
+		popup = new StringDisplay();
 	}
 	
 	public void start()
 	{
 		Kahoot myFirstKahoot = new Kahoot();
 		myKahoots.add(myFirstKahoot);
+		fillTheList();
+		showTheList();
 	}
 	
+	private void showTheList()
+	{
+		for (int index = 0 ; index < myKahoots.size(); index ++)
+		{
+			popup.displayText(myKahoots.get(index).toString());
+		}
+	}
+	/*
+	 * added a lot of objects to the list
+	 */
 	private void fillTheList()
 	{
 		Kahoot fiftyStates = new Kahoot("kashish", 50);
-		Kahoot mySecondahoot =new Kahoot("Ethan", 2);
+		Kahoot mySecondahoot = new Kahoot("Ethan", 2);
 		Kahoot isaacStinks = new Kahoot("Issac", 1);
 		Kahoot bigQuiz = new Kahoot("Derek", Integer.MAX_VALUE);
 		Kahoot animalColor = new Kahoot("Branton", 10);
