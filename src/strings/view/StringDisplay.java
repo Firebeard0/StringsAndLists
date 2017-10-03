12d6dae78c6edddbd883d56a842c4215e6b85c4d
Firebeard0 <31518739+Firebeard0@users.noreply.github.com>
2017-10-03 12:29:32 -0600

    added popup display class

--- src/strings/view/StringDisplay.java
+++ src/strings/view/StringDisplay.java
@@ -1,6 +1,19 @@
 package strings.view;
 
+import javax.swing.JOptionPane;
 public class StringDisplay
 {
-
+	public void displayText(String textToDisplay)
+	
+	{
+		JOptionPane.showMessageDialog(null, textToDisplay);
+	}
+	
+	public String getResponse(String fromQuestion)
+	{
+		String answer = "";
+		answer += JOptionPane.showInputDialog(null, fromQuestion);
+		
+		return answer;
+	}
 }
